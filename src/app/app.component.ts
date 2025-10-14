c27357a887aa40d4566f4943b444a815b9899414
Karan02 <krnptl1234@gmail.com>
2025-10-14 23:19:04 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, effect, inject, signal } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  clickCount = signal(0);
+  private destroyRef = inject(DestroyRef);
+  constructor(){
+    effect(() =>{
+      console.log(`clicked button ${this.clickCount()} times.`)
+    });
+  }
 
+  //Observables are great for managing events and streamed data
+  //Signals are great for managing application state
+
+  ngOnInit(): void {
+
+      //Observable
+      // const subscription = interval(1000).pipe( // pipe are inserted into streamline to manipulate data
+      //   map((val) => val * 2), // operator
+      // ).subscribe({
+      //   next: (val) => console.log(val),
+      // }); //every second, observable
+      // this.destroyRef.onDestroy(() =>{
+      //   subscription.unsubscribe();
+      // })
+    }
+
+    //signal
+  OnClick(){
+      this.clickCount.update(prevCount => prevCount + 1);
+    }
 }
